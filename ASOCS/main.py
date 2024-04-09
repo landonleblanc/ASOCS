7057919169aae36f0721fae2329e8b54e36d8ef2
Landon LeBlanc <landonleblanc@gmail.com>
2024-04-09 16:18:34 -0700

    Replaced MAX6675 class with driver module

--- ASOCS/main.py
+++ ASOCS/main.py
@@ -5,68 +5,7 @@ import digitalio
 import adafruit_ds3231
 import adafruit_ssd1306
 from simple_pid import PID
-
-class MAX6675:
-    MEASUREMENT_PERIOD_S = 0.22
-
-    def __init__(self, sck, cs, so):
-        self._sck = digitalio.DigitalInOut(sck)
-        self._sck.direction = digitalio.Direction.OUTPUT
-        self._sck.value = False
-
-        self._cs = digitalio.DigitalInOut(cs)
-        self._cs.direction = digitalio.Direction.OUTPUT
-        self._cs.value = True
-
-        self._so = digitalio.DigitalInOut(so)
-        self._so.direction = digitalio.Direction.INPUT
-
-        self._last_measurement_start = 0
-        self._last_read_temp = 0
-        self._error = 0
-
-    def _cycle_sck(self):
-        self._sck.value = True
-        time.sleep(0.000001)
-        self._sck.value = False
-        time.sleep(0.000001)
-
-    def refresh(self):
-        self._cs.value = False
-        time.sleep(0.00001)
-        self._cs.value = True
-        self._last_measurement_start = time.monotonic()
-
-    def ready(self):
-        return time.monotonic() - self._last_measurement_start > self.MEASUREMENT_PERIOD_S
-
-    def error(self):
-        return self._error
-
-    def read(self):
-        if self.ready():
-            self._cs.value = False
-            time.sleep(0.00001)
-
-            value = 0
-            for i in range(12):
-                self._cycle_sck()
-                value += self._so.value << (11 - i)
-
-            self._cycle_sck()
-            self._error = self._so.value
-
-            for i in range(2):
-                self._cycle_sck()
-
-            self._cs.value = True
-            self._last_measurement_start = time.monotonic()
-
-            self._last_read_temp = value * 0.25
-
-        return self._last_read_temp
-# Usage:
-# tc = MAX6675(board.SCK, board.D5, board.MISO)
+from max6675 import MAX6675
 
 def get_date_time():
     raw_time = rtc.datetime#retrieve time from rtc
@@ -78,6 +17,7 @@ def get_date_time():
     return formatted_time
 
 def set_time():
+    #TODO: Update to be updated via ui
     if rtc.datetime.tm_year == 0: #users sets the time if there isn't one
         print('Enter the current date and time in the following format then press enter:\n YYYY,MM,DD,HH,MM,SS,WDAY,DOY')
         t = input()
@@ -130,17 +70,15 @@ def debug_log():
 def data_log():
     pass
 
-
-print('Initializing I2C...')
+#Startup Sequence
 rtc_i2c = busio.I2C(board.GP19, board.GP18)#create an i2c object on pins 18 and 19
-rtc_oled = busio.I2C(board.GP13, board.GP12)#create an i2c object on pins 18 and 19
-print('I2C initialized')
+oled_i2c = busio.I2C(board.GP13, board.GP12)#create an i2c object on pins 18 and 19
 print('Initializing RTC...')
 rtc = adafruit_ds3231.DS3231(rtc_i2c)#initialize the ds3231
 set_time()#set the time if it has defaulted
 print('RTC initialized')
 print('Initializing OLED...')
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, rtc_oled)#initialize the lcd
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, oled_i2c)#initialize the lcd
 oled.fill(1)
 oled.show()
 time.sleep(0.5)
@@ -160,7 +98,6 @@ control_temp = 50
 start_time = 11
 end_time = 17
 control_enabled = False
-#create a PID object
 pid = PID(1, 0.1, 0.05, setpoint=control_temp)
 relay = digitalio.DigitalInOut(board.GP28)
 relay.direction = digitalio.Direction.OUTPUT
