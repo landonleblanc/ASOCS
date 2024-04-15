c1ab67d1901284fcaeca2ec7ce6d7feb28c0efa9
Landon <landonleblanc@gmail.com>
2024-04-14 20:45:07 -0700

    split some functions for better modularity

--- ASOCS/main.py
+++ ASOCS/main.py
@@ -7,17 +7,7 @@ import adafruit_ssd1306
 from simple_pid import PID
 from max6675 import MAX6675
 
-def get_date_time():
-    raw_time = rtc.datetime#retrieve time from rtc
-    print(raw_time)
-    formatted_time = [raw_time.tm_year, raw_time.tm_mon, raw_time.tm_mday, raw_time.tm_hour, raw_time.tm_min] #save the data we want to a list
-    for index, i in enumerate(formatted_time):  # add a leading zero to times less than 10
-        if i < 10:
-            formatted_time[index] = f'0{i}'
-    formatted_time = [f'{formatted_time[0]}/{formatted_time[1]}/{formatted_time[2]}', f'{formatted_time[3]}:{formatted_time[4]}'] #format the time into a list, date and time
-    return formatted_time
-
-def set_time():
+def set_time(rtc):
     #TODO: Update to be updated via ui
     if rtc.datetime.tm_year == 0: #users sets the time if there isn't one
         print('Enter the current date and time in the following format then press enter:\n YYYY,MM,DD,HH,MM,SS,WDAY,DOY')
@@ -68,70 +58,91 @@ def debug_log():
 def data_log():
     pass
 
-#Hardware Startup Sequence
-rtc_i2c = busio.I2C(board.GP19, board.GP18)#create an i2c object on pins 18 and 19
-oled_i2c = busio.I2C(board.GP13, board.GP12)#create an i2c object on pins 18 and 19
-print('Initializing OLED...')
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, oled_i2c)#initialize the lcd
-oled.fill(1)
-oled.show()
-time.sleep(0.5)
-oled.fill(0)
-oled.show()
-print('OLED initialized')
-oled.text('Initializing', 0, 0, 1)
-oled.text('System...', 0, 10, 1)
-oled.show()
-print('Initializing RTC...')
-rtc = adafruit_ds3231.DS3231(rtc_i2c)#initialize the ds3231
-set_time()#set the time if it has defaulted
-print('RTC initialized')
-print('Initializing thermocouple...')
-tc = MAX6675(board.GP2, board.GP3, board.GP4)
-print('Thermocouple initialized')
-time.sleep(1)
-oled.text('System', 0, 0, 1)
-oled.text('Initialized', 0, 10, 1)
-oled.show()
-oled.fill(0)
-oled.show()
-#End Hardware Startup Sequence
+def reset_oled(oled):
+    oled.fill(0)
+    oled.show()
+    return
 
-control_temp = 50
-start_time = 11
-end_time = 17
-control_enabled = False
-pid = PID(1, 0.1, 0.05, setpoint=control_temp)
-relay = digitalio.DigitalInOut(board.GP28)
-relay.direction = digitalio.Direction.OUTPUT
-pid.auto_mode = True
-pid.output_limits = (0, 10)
-element_on_until = 0
-#data dict for date, time, air temp, oven temp, if PID is needed, and relay status
-data = {'date': '', 'time': '', 'air': 15, 'oven': 15, 'pid': False , 'relay': False}
-while True:
-    data['date'] = get_date_time()[0]
-    data['time'] = get_date_time()[1]
-    data['air'] = rtc.temperature #obtain the air temp from the rtc
-    data['oven'] = tc.read() #obtain the oven temp from the thermocouple
-    if data['pid'] == False:
-        data['pid'] = need_pid(data['oven'], data['time']) #determine if the oven needs to be controlled
-    elif data['pid'] == True:
-        pid_output = pid(data['oven']) #if control is needed, run the PID
-        print(f'PID output: {pid_output}')
-        if pid_output > 0.5:
-            data['relay'] = True
+def init_hw():
+    #Hardware Startup Sequence
+    rtc_i2c = busio.I2C(board.GP19, board.GP18)#create an i2c object on pins 18 and 19
+    oled_i2c = busio.I2C(board.GP13, board.GP12)#create an i2c object on pins 18 and 19
+    print('Initializing OLED...')
+    oled = adafruit_ssd1306.SSD1306_I2C(128, 64, oled_i2c)#initialize the lcd
+    oled.fill(1)
+    oled.show()
+    time.sleep(0.5)
+    reset_oled(oled)
+    print('OLED initialized')
+    oled.text('Initializing', 0, 0, 1)
+    oled.text('System...', 0, 10, 1)
+    oled.show()
+    print('Initializing RTC...')
+    rtc = adafruit_ds3231.DS3231(rtc_i2c)#initialize the ds3231
+    set_time()#set the time if it has defaulted
+    print('RTC initialized')
+    print('Initializing thermocouple...')
+    tc = MAX6675(board.GP2, board.GP3, board.GP4)
+    print('Thermocouple initialized')
+    print('Initializing relay...')
+    relay = digitalio.DigitalInOut(board.GP28)
+    relay.direction = digitalio.Direction.OUTPUT
+    print('Relay initialized')
+    time.sleep(1)
+    oled.text('System', 0, 0, 1)
+    oled.text('Initialized', 0, 10, 1)
+    oled.show()
+    reset_oled(oled)
+    #End Hardware Startup Sequence
+    return rtc, oled, tc, relay
+
+def system_settings():
+    pass
+
+def init_pid(temp):
+    pid = PID(1, 0.1, 0.05, setpoint=temp)
+    pid.auto_mode = True
+    pid.output_limits = (0, 10)
+    return pid
+
+
+
+def main():
+    control_temp = 50
+    start_time = 11
+    end_time = 17
+    control = False
+    element_on_until = 0
+    time = 0
+    data = {'air': 15, 'oven': 15, 'pid_needed': False , 'element': False, 'pid': 0}
+    rtc, oled, tc, relay = init_hw()
+    pid = init_pid(control_temp)
+    while True:
+        time = rtc.datetime
+        data['air'] = rtc.temperature #obtain the air temp from the rtc
+        data['oven'] = tc.read() #obtain the oven temp from the thermocouple
+        if data['pid'] == False:
+            data['pid'] = need_pid(data['oven'], data['time']) #determine if the oven needs to be controlled
+        elif data['pid'] == True:
+            pid_output = pid(data['oven']) #if control is needed, run the PID
+            print(f'PID output: {pid_output}')
+            if pid_output > 0.5:
+                data['relay'] = True
+            else:
+                data['relay'] = False
         else:
             data['relay'] = False
-    else:
-        data['relay'] = False
-    relay.value = data['relay'] #set the relay to the PID output
-    hour = data['time'].split(':')[0]
-    if int(hour) > 17:
-        data['pid'] = False
-    print(f'Date: {data["date"]}, Time: {data["time"]}, Air Temp: {data["air"]}C, Oven Temp: {data["oven"]}C, PID Needed?: {data["pid"]}, Relay Status: {data["relay"]}')
-    update_oled(oled, data)
-    time.sleep(0.01)
-#TODO: Remove data dictionary and use global variables
+        relay.value = data['relay'] #set the relay to the PID output
+        hour = data['time'].split(':')[0]
+        if int(hour) > 17:
+            data['pid'] = False
+        print(f'Date: {data["date"]}, Time: {data["time"]}, Air Temp: {data["air"]}C, Oven Temp: {data["oven"]}C, PID Needed?: {data["pid"]}, Relay Status: {data["relay"]}')
+        update_oled(oled, data)
+        time.sleep(0.01)
+    
+
+if __name__ == '__main__':
+    main()
 #TODO: Add pid output = some sort of time on for the relay
 #TODO: Update get_date_time to use use epoch time instead of a formatted string. Only format when needed
+#TODO: Store any user editable params in settings.json
